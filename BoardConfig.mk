468f26312235559c900e9c90a3125c785773a262
deadman96385 <seanhoyt963@gmail.com>
2019-05-12 04:08:56 -0500

    Initital commit

---
+++ BoardConfig.mk
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/nokia/dynamo2
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.memcg=true user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_KERNEL_APPEND_DTB := true
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_ARCH := arm
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androidkernel-
+TARGET_KERNEL_CONFIG := E2M-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/nokia/msm8937
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1395863552
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5593291776
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 29360128
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_VENDORIMAGE_PARTITION_SIZE := 335544320
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# TWRP Configuration
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_EXTRA_LANGUAGES := true
+TW_THEME := portrait_hdpi
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# Disable Mouse Cursor
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
---
+++ README.md
@@ -0,0 +1,24 @@
+Device configuration for Nokia 2.1 (dynamo2)
+=========================================
+
+The Nokia 2.1 (codenamed _"dynamo2"_) is a low-mid range smartphone from HMD/Nokia.
+
+It was announced in May 2018. Release date was August 2018.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+SoC     | Qualcomm MSM8917 Snapdragon 425
+CPU     | Quad-core (4x1.4 GHz Cortex-A53)
+GPU     | Adreno 308
+Memory  | 1 GB RAM
+Shipped Android Version | 8.1
+Storage | 8 GB
+Battery | Non-Removable Li-Ion 4000mAh battery
+Display | 1280 x 720 pixels, 16:9 ratio 5.5 inches, IPS LCD (~267 ppi density)
+Camera  | 8 MP, LED flash
+
+## Device picture
+
+![Nokia 2.1](http://www.advancetelecom.com.pk/wp-content/uploads/2018/08/nokia-n.1_1-600x600.png "Nokia 2.1")
